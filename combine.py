f78431cb23ebdcc6398cafc980bec9cee11df574
Niloux3d <15072763003@163.com>
2025-11-26 09:29:21 +0800

    feat(test_lidar): 重构激光雷达测试脚本并添加可视化功能
    重构测试脚本以支持从模型加载高斯数据，并添加全景图和鸟瞰图可视化功能
    移除旧的随机高斯生成方法，改为从GSModel加载数据
    添加save_rendered_feat_panorama和save_bev_from_rendered_feat函数用于结果可视化

---
+++ combine.py
@@ -0,0 +1,26 @@
+import os
+from typing import List
+
+from config import DEVICE
+from models import GaussianComponent, GSModel
+
+os.environ["TORCH_CUDA_ARCH_LIST"] = "12.0"
+
+device = DEVICE
+model1 = GSModel.load_from_pth("121.pth").to_device(device)
+background: GaussianComponent = model1.get_component("background")
+sky: GaussianComponent = model1.get_component("sky")
+actors: List[GaussianComponent] = model1.get_components_by_type("obj")
+names = [actor.name for actor in actors]
+print(f"{names=}")  # 010,016,034是效果还ok的, 下标对应2, 5, 9
+
+actor1: GaussianComponent = actors[1]
+actor2: GaussianComponent = actors[5]
+actor3: GaussianComponent = actors[9]
+
+
+model2 = GSModel.load_from_pth("049_1009.pth").to_device(device)
+model2.add_component(actor1)
+model2.add_component(actor2)
+model2.add_component(actor3)
+model2.save_to_pth("049_1009_conbined.pth")
